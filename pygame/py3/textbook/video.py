cf38c8868140db8af2c4824a58362f7b9a879f12
Andrey <tensesamsu@gmail.com>
2024-03-20 15:20:23 +0900

    py2-5

--- pygame/py3/textbook/video.py
+++ pygame/py3/textbook/video.py
@@ -4,6 +4,30 @@ from new import Board as br
 
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 501, 501
 FPS = 15
+MAP_DIR = 'maps'
+TILE_SIZE = 32
+
+
+class Labyrinth:
+    def __init__(self, filename, free_tiles, finish_tile):
+        self.map = []
+        with open(f"{MAP_DIR}/{filename}") as input_file:
+            for line in input_file:
+                self.map.append(list(map(int, line.split())))
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.tile_size = TILE_SIZE
+        self.free_tiles = free_tiles
+        self.finish_tile = finish_tile
+
+    def render(self, screen):
+        colors = {0: (0, 0, 0), 1: (120, 120, 120), 2: (50, 50, 50)}
+        for y in range(self.height):
+            for x in range(self.width):
+                rect = pygame.Rect(x * self.tile_size, y * self.tile_size,
+                                   (self.tile_size, self.tile_size))
+                screen.fill(colors[self.map[y][x]], rect)
+
 
 
 def main():
@@ -31,3 +55,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
