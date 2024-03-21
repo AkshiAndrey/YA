3cdb0fba824a7cb32ec6f49666c76e76d0271fa4
Andrey <tensesamsu@gmail.com>
2024-03-21 17:01:49 +0900

    p4_1

---
+++ pygame/py4/task1.py
@@ -0,0 +1,142 @@
+import pygame
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 501, 501
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # self.board_color = [[0] * width for _ in range(height)]
+        self.color = (255, 255, 255)
+        self.left = 10
+        self.top = 10
+        self.colors = {1: (255, 0, 0), 2: (0, 0, 255)}
+        self.cell_size = 30
+        self.turn_status = 1
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+
+        for i in range(self.height):
+            for j in range(self.width):
+                pygame.draw.rect(screen, self.color,
+                                 (self.top + self.cell_size * j, self.left + self.cell_size * i,
+                                  self.cell_size, self.cell_size), 1)
+                if color := self.board[i][j]:
+                    if color == 1:
+                        pygame.draw.circle(screen, self.colors[color],
+                                           (self.top + self.cell_size * j + self.cell_size / 2,
+                                            self.left + self.cell_size * i + self.cell_size / 2),
+                                           self.cell_size / 2 - 5, 1)
+                    else:
+                        pygame.draw.line(screen, self.colors[color],
+                                         (self.top + self.cell_size * j + 2,
+                                          self.left + self.cell_size * i + 2),
+                                         (self.top + self.cell_size * j - 2 + self.cell_size,
+                                          self.left + self.cell_size * i - 2 + self.cell_size), 1)
+                        pygame.draw.line(screen, self.colors[color],
+                                         (self.top + self.cell_size * j + self.cell_size - 2,
+                                          self.left + self.cell_size * i + 2),
+                                         (self.top + self.cell_size * j + 2,
+                                          self.left + self.cell_size * i - 2 + self.cell_size), 1)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def get_cell(self, mouse_pos):
+        for i, value in enumerate(self.board):
+            for j, value_ in enumerate(value):
+                height = self.cell_size * (i + 1) + self.top
+                width = self.cell_size * (j + 1) + self.left
+                width_min = width - self.cell_size
+                height_min = height - self.cell_size
+                if width_min < mouse_pos[0] < width and height_min < mouse_pos[1] < height:
+                    return i, j
+        return None
+
+    def on_click(self, mouse_pos):
+        if mouse_pos:
+            if not self.board[mouse_pos[0]][mouse_pos[1]]:
+                self.board[mouse_pos[0]][mouse_pos[1]] = self.turn_status
+                self.turn_status = 1 if self.turn_status == 2 else 2
+
+
+class Lines(Board):
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.flag_red_circle = []
+
+    def has_path(self, x1, y1, x2, y2):
+        start = x1, x2
+        if (x1, y1) == (x2, y2):
+            self.board[x1][y1] = 2
+            self.board[start[0]][start[1]] = 0
+            self.flag_red_circle = []
+            return True
+        if self.board[x1 + 1][y1]
+        pass
+
+    def on_click(self, mouse_pos):
+        if mouse_pos:
+            if self.board[mouse_pos[0]][mouse_pos[1]] == 0:
+                if not self.flag_red_circle:
+                    self.board[mouse_pos[0]][mouse_pos[1]] = 2
+                else:
+                    self.has_path(*mouse_pos, *self.flag_red_circle)
+                    pygame.display.quit()
+            elif self.board[mouse_pos[0]][mouse_pos[1]] == 2:
+                self.board[mouse_pos[0]][mouse_pos[1]] = 1
+                self.flag_red_circle = mouse_pos
+            elif self.board[mouse_pos[0]][mouse_pos[1]] == 1:
+                self.board[mouse_pos[0]][mouse_pos[1]] = 2
+                self.flag_red_circle = []
+
+    def render(self, screen):
+
+        for i in range(self.height):
+            for j in range(self.width):
+                pygame.draw.rect(screen, self.color,
+                                 (self.top + self.cell_size * j, self.left + self.cell_size * i,
+                                  self.cell_size, self.cell_size), 1)
+                if color := self.board[i][j]:
+                    if color == 1:
+                        pygame.draw.circle(screen, self.colors[color],
+                                           (self.top + self.cell_size * j + self.cell_size / 2,
+                                            self.left + self.cell_size * i + self.cell_size / 2),
+                                           self.cell_size / 2 - 5)
+                    else:
+                        pygame.draw.circle(screen, self.colors[color],
+                                           (self.top + self.cell_size * j + self.cell_size / 2,
+                                            self.left + self.cell_size * i + self.cell_size / 2),
+                                           self.cell_size / 2 - 5)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    lines = Lines(10, 10)
+    # board.set_view(5, 5, 80)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                lines.get_click(event.pos)
+        screen.fill((0, 0, 0))
+        lines.render(screen)
+        pygame.display.flip()
+
+    pygame.display.quit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pygame/py4/test.py
@@ -0,0 +1,3 @@
+flag_red_circle = []
+if flag_red_circle:
+    print(flag_red_circle)
