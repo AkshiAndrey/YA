cdc3e5c8900166e24b084257fa1990f8580e409e
Andrey Kylkov <tensesamsu@gmail.com>
2024-03-22 00:11:12 +0900

    task1 py4

--- pygame/py4/task1.py
+++ pygame/py4/task1.py
@@ -1,4 +1,7 @@
+from collections import deque
+
 import pygame
+import copy
 
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 501, 501
 
@@ -7,8 +10,7 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        # self.board_color = [[0] * width for _ in range(height)]
+        self.board = [[0 for i in range(width)] for _ in range(height)]
         self.color = (255, 255, 255)
         self.left = 10
         self.top = 10
@@ -73,31 +75,89 @@ class Lines(Board):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.flag_red_circle = []
+        self.board_clone = []
+
+
+
+
+
+    # def has_path(self, x1, y1, x2, y2):
+    #     self.board_clone[x2][y2] = 1
+    #     if (x2, y2) == (x1, y1):
+    #         return True
+    #     if self.exist_sell(x2 + 1, y2):
+    #         if self.board_clone[x2 + 1][y2] == 0:
+    #             # self.board_clone[x2 + 1][y2] = 1
+    #             return self.has_path(x1, y1, x2 + 1, y2)
+    #     if self.exist_sell(x2, y2 + 1):
+    #         if self.board_clone[x2][y2 + 1] == 0:
+    #             # self.board_clone[x2][y2 + 1] = 1
+    #             return self.has_path(x1, y1, x2, y2 + 1)
+    #     if self.exist_sell(x2 - 1, y2):
+    #         if self.board_clone[x2 - 1][y2] == 0:
+    #             # self.board_clone[x2 - 1][y2] = 1
+    #             return self.has_path(x1, y1, x2 - 1, y2)
+    #     if self.exist_sell(x2, y2 - 1):
+    #         if self.board_clone[x2][y2 - 1] == 0:
+    #             # self.board_clone[x2][y2 - 1] = 1
+    #             return self.has_path(x1, y1, x2, y2 - 1)
+    #     return False
+
 
     def has_path(self, x1, y1, x2, y2):
-        start = x1, x2
-        if (x1, y1) == (x2, y2):
-            self.board[x1][y1] = 2
-            self.board[start[0]][start[1]] = 0
-            self.flag_red_circle = []
-            return True
-        if self.board[x1 + 1][y1]
-        pass
+        rows, cols = len(self.board), len(self.board[0])
+
+        queue = deque([(x2, y2)])
+        visited = set()
+        visited.add((x2, y2))
+
+        while queue:
+            current_cell = queue.popleft()
+            if current_cell == (x1, y1):
+                return True
+
+            neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # Possible movements: down, up, right, left
+
+            for dr, dc in neighbors:
+                new_row, new_col = current_cell[0] + dr, current_cell[1] + dc
+
+                if 0 <= new_row < rows and 0 <= new_col < cols and self.board[new_row][new_col] == 1 and (
+                        new_row, new_col) not in visited:
+                    visited.add((new_row, new_col))
+
+                elif 0 <= new_row < rows and 0 <= new_col < cols and self.board[new_row][new_col] != 1 and (
+                        new_row, new_col) not in visited:
+                    queue.append((new_row, new_col))
+                    visited.add((new_row, new_col))
+        return False
 
     def on_click(self, mouse_pos):
+        self.board_clone = copy.deepcopy(self.board)
         if mouse_pos:
             if self.board[mouse_pos[0]][mouse_pos[1]] == 0:
                 if not self.flag_red_circle:
                     self.board[mouse_pos[0]][mouse_pos[1]] = 2
                 else:
-                    self.has_path(*mouse_pos, *self.flag_red_circle)
-                    pygame.display.quit()
+                    if self.has_path(*mouse_pos, *self.flag_red_circle):
+                        self.board[mouse_pos[0]][mouse_pos[1]] = 2
+                        self.board[self.flag_red_circle[0]][self.flag_red_circle[1]] = 0
+                        self.flag_red_circle = []
             elif self.board[mouse_pos[0]][mouse_pos[1]] == 2:
                 self.board[mouse_pos[0]][mouse_pos[1]] = 1
                 self.flag_red_circle = mouse_pos
             elif self.board[mouse_pos[0]][mouse_pos[1]] == 1:
                 self.board[mouse_pos[0]][mouse_pos[1]] = 2
                 self.flag_red_circle = []
+
+    def exist_sell(self, x, y):
+        ex = False
+        try:
+            ex = self.board_clone[x][y]
+            if ex == 0:
+                return True
+        except Exception:
+            pass
+        return ex
 
     def render(self, screen):
 
@@ -113,17 +173,18 @@ class Lines(Board):
                                             self.left + self.cell_size * i + self.cell_size / 2),
                                            self.cell_size / 2 - 5)
                     else:
-                        pygame.draw.circle(screen, self.colors[color],
-                                           (self.top + self.cell_size * j + self.cell_size / 2,
-                                            self.left + self.cell_size * i + self.cell_size / 2),
-                                           self.cell_size / 2 - 5)
+                        if color == 2:
+                            pygame.draw.circle(screen, self.colors[color],
+                                               (self.top + self.cell_size * j + self.cell_size / 2,
+                                                self.left + self.cell_size * i + self.cell_size / 2),
+                                               self.cell_size / 2 - 5)
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    lines = Lines(10, 10)
-    # board.set_view(5, 5, 80)
+    lines = Lines(5, 5)
+    # lines.set_view(2, 2, 80)
     running = True
     while running:
         for event in pygame.event.get():
